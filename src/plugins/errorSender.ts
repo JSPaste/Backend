344d9cc8aeffd79581c7e6fbc21b8ae45bcc025f
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-01-07 21:52:41 +0000

    error sender plugin

---
+++ src/plugins/errorSender.ts
@@ -0,0 +1,10 @@
+import Elysia from 'elysia';
+import { ErrorSender } from '../classes/ErrorSender';
+
+export const errorSenderPlugin = new Elysia({
+	name: 'plugins:errorHandler',
+}).derive((context) => {
+	return {
+		errorSender: new ErrorSender(context),
+	};
+});
