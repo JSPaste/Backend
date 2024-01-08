1c7ad3655c0a50a3ea61fb37dc1be75b4b71a28b
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-01-08 19:27:37 +0000

    fix index routes

--- src/routes/v1/index.route.ts
+++ src/routes/v1/index.route.ts
@@ -2,6 +2,6 @@ import { Elysia } from 'elysia';
 
 export default new Elysia({
 	name: 'routes:v1:documents',
-}).post('', () => {
+}).get('', () => {
 	return 'Welcome to JSPaste API v1';
 });
--- src/routes/v2/index.route.ts
+++ src/routes/v2/index.route.ts
@@ -2,6 +2,6 @@ import { Elysia } from 'elysia';
 
 export default new Elysia({
 	name: 'routes:v2:documents',
-}).post('', () => {
+}).get('', () => {
 	return 'Welcome to JSPaste API v2';
 });
