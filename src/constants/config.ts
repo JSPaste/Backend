c16c5bbe328a62885876bf3d588fe876ce7bdd7f
Mrgaton <arnaupost4@gmail.com>
2024-01-12 20:11:15 +0100

    Set default doc size to 2Mb

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,3 @@
 
 export const basePath = process.env['DOCUMENTS_PATH'] ?? 'documents/';
-export const maxDocLength = parseInt(process.env['MAX_FILE_LENGTH'] ?? '50000');
+export const maxDocLength = parseInt(process.env['MAX_FILE_LENGTH'] ?? '2000000');
