ebd949832c68505cf7771c1bc34f9515fb0fab3c
Ivan Gabaldon <git@inetol.net>
2024-02-01 19:03:28 +0100

    fix createSecret

--- src/utils/createSecret.ts
+++ src/utils/createSecret.ts
@@ -1,5 +1,7 @@
 import { randomString } from './randomString.ts';
 
 export async function createSecret(chunkLength: number = 5, chunks: number = 4): Promise<string> {
-	return Array.from({ length: chunks }, () => randomString(chunkLength)).join('-');
+	const secret = await Promise.all(Array.from({ length: chunks }, () => randomString(chunkLength)));
+
+	return secret.join('-');
 }
