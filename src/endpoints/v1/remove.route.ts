ca3003c859be05acb6a030c0d998617439b021c0
Ivan Gabaldon <git@inetol.net>
2024-05-29 23:53:53 +0200

    migrate removeV1

--- src/endpoints/v1/remove.route.ts
+++ src/endpoints/v1/remove.route.ts
@@ -1,16 +1,56 @@
 import { unlink } from 'node:fs/promises';
-import type { Hono } from 'hono';
+import { type OpenAPIHono, createRoute, z } from '@hono/zod-openapi';
 import { storage } from '../../document/storage.ts';
 import { validator } from '../../document/validator.ts';
+import { schema } from '../../errorHandler.ts';
 import { config } from '../../server.ts';
 
-export const removeRoute = (endpoint: Hono) => {
-	endpoint.delete('/:name', async (ctx) => {
-		const params = ctx.req.param();
+export const removeRoute = (endpoint: OpenAPIHono) => {
+	const route = createRoute({
+		method: 'delete',
+		path: '/{name}',
+		tags: ['v1'],
+		summary: 'Delete a document',
+		deprecated: true,
+		request: {
+			params: z.object({
+				name: z
+					.string()
+					.min(config.DOCUMENT_NAME_LENGTH_MIN)
+					.max(config.DOCUMENT_NAME_LENGTH_MAX)
+					.openapi({
+						description: 'The document name',
+						examples: ['abc123']
+					})
+			}),
+			headers: z.object({
+				secret: z.string().min(1).openapi({
+					description: 'The secret key'
+				})
+			})
+		},
+		responses: {
+			200: {
+				content: {
+					'application/json': {
+						schema: z.object({
+							removed: z.boolean({ description: 'Confirmation of deletion' }).openapi({
+								example: true
+							})
+						})
+					}
+				},
+				description: 'Confirmation of deletion'
+			},
+			400: schema,
+			404: schema,
+			500: schema
+		}
+	});
 
-		const headers = {
-			secret: ctx.req.header('secret')
-		};
+	endpoint.openapi(route, async (ctx) => {
+		const params = ctx.req.valid('param');
+		const headers = ctx.req.valid('header');
 
 		const document = await storage.read(params.name);
 
--- src/endpoints/v2/remove.route.ts
+++ src/endpoints/v2/remove.route.ts
@@ -1,6 +1,8 @@
-import type { Hono } from 'hono';
+/* TODO: Migrate
 import { removeRoute as removeRouteV1 } from '../../endpoints/v1/remove.route.ts';
+import type { OpenAPIHono } from '@hono/zod-openapi';
 
-export const removeRoute = (endpoint: Hono) => {
+export const removeRoute = (endpoint: OpenAPIHono) => {
 	removeRouteV1(endpoint);
 };
+*/
