87d70e1e79d9f21334db02cbad68b0f72b11a8b2
Ivan Gabaldon <git@inetol.net>
2024-06-05 14:36:46 +0200

    fix #51 (again)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { env, server } from './server.ts';
 
+// TODO: Support graceful shutdown
+process.on('SIGTERM', () => process.exit(0));
+
 export default {
 	port: env.port,
 	fetch: server().fetch
