4e81a1f667f2465aae140223f8f8020025afd1d9
Mrgaton <arnaupost4@gmail.com>
2024-07-31 00:38:35 +0200

    update bun.lockb, package.json, compression.ts and storage.ts

--- src/document/compression.ts
+++ src/document/compression.ts
@@ -1,11 +1,19 @@
-import { type InputType, brotliCompress, brotliDecompress } from 'node:zlib';
+import zlib, { type InputType, brotliCompress, brotliDecompress } from 'node:zlib';
 import { errorHandler } from '../server/errorHandler.ts';
 import { ErrorCode } from '../types/ErrorHandler.ts';
+
+const compressOptions = {
+	params: {
+		[zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT, // 0 = generic, 1 = text, 2 = font (WOFF2)
+		[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+		[zlib.constants.BROTLI_PARAM_LGWIN]: 28 // 256mb
+	}
+};
 
 export const compression = {
 	encode: (data: InputType): Promise<Buffer> => {
 		return new Promise((resolve, reject) => {
-			brotliCompress(data, (err, buffer) => {
+			brotliCompress(data, compressOptions, (err, buffer) => {
 				if (err) {
 					reject(errorHandler.send(ErrorCode.documentCorrupted));
 				} else {
