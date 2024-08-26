cdf9abd428919e9cd34bf62a3493f367e8c53bcc
Mrgaton <68958481+Mrgaton@users.noreply.github.com>
2024-08-26 22:02:45 +0200

    Brotli compression options and jsc serealizer (#147)
    Co-authored-by: Ivan Gabaldon <git@inetol.net>

--- src/document/compression.ts
+++ src/document/compression.ts
@@ -1,11 +1,24 @@
-import { type InputType, brotliCompress, brotliDecompress } from 'node:zlib';
+import { brotliCompress, brotliDecompress, constants as zlibConstants } from 'node:zlib';
 import { errorHandler } from '../server/errorHandler.ts';
 import { ErrorCode } from '../types/ErrorHandler.ts';
 
+const compressOptions: { [key: number]: number } = {
+	[zlibConstants.BROTLI_PARAM_QUALITY]: 11,
+	[zlibConstants.BROTLI_PARAM_LGWIN]: 28 // 256mb
+};
+
 export const compression = {
-	encode: (data: InputType): Promise<Buffer> => {
+	encode: (data: ArrayBuffer): Promise<Buffer> => {
 		return new Promise((resolve, reject) => {
-			brotliCompress(data, (err, buffer) => {
+			const isText = Buffer.from(data.slice(0, 1024 * 2)).every(
+				(byte) => (byte >= 32 && byte <= 126) || byte === 9 || byte === 10 || byte === 13
+			);
+
+			compressOptions[zlibConstants.BROTLI_PARAM_MODE] = isText
+				? zlibConstants.BROTLI_MODE_TEXT
+				: zlibConstants.BROTLI_MODE_GENERIC;
+
+			brotliCompress(data, compressOptions, (err, buffer) => {
 				if (err) {
 					reject(errorHandler.send(ErrorCode.documentCorrupted));
 				} else {
@@ -15,7 +28,7 @@ export const compression = {
 		});
 	},
 
-	decode: (data: InputType): Promise<Buffer> => {
+	decode: (data: Uint8Array): Promise<Buffer> => {
 		return new Promise((resolve, reject) => {
 			brotliDecompress(data, (err, buffer) => {
 				if (err) {
