e89680746d78b7de0c27a1d0706861cc78c3b365
Mrgaton <arnaupost4@gmail.com>
2024-10-19 23:19:16 +0200

    Use hmac for password salt

--- src/document/crypto.ts
+++ src/document/crypto.ts
@@ -2,18 +2,17 @@ import { randomBytes } from 'node:crypto';
 
 const hashAlgorithm = 'blake2b256';
 const saltLength = 16;
-
 export const crypto = {
 	hash: (password: string): Uint8Array => {
 		const salt = randomBytes(saltLength);
-		const hasher = new Bun.CryptoHasher(hashAlgorithm).update(salt).update(password);
+		const hasher = new Bun.CryptoHasher(hashAlgorithm, salt).update(password);
 
 		return Buffer.concat([salt, hasher.digest()]);
 	},
 
 	compare: (password: string, hash: Uint8Array): boolean => {
 		const salt = hash.subarray(0, saltLength);
-		const hasher = new Bun.CryptoHasher(hashAlgorithm).update(salt).update(password);
+		const hasher = new Bun.CryptoHasher(hashAlgorithm, salt).update(password);
 
 		const passwordHash = Buffer.concat([salt, hasher.digest()]);
 
