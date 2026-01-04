66b650740fbac4a7ed61ef617f42056848f76e12
Ivan Gabaldon <igabaldon@inetol.net>
2026-01-04 21:29:23 +0100

    validator documentation improvements

--- src/utils/validator/document.ts
+++ src/utils/validator/document.ts
@@ -1,23 +1,24 @@
 import { type } from "arktype";
 import { constant } from "#/global.ts";
+import { regexBase64URL } from "./regex.ts";
 
-export const validatorDocumentName = type(/^[A-Za-z0-9_-]+$/)
+export const validatorDocumentName = type(regexBase64URL)
   .atLeastLength(constant.documentNameLengthMin)
   .atMostLength(constant.documentNameLengthMax)
   .configure({
     ref: "DocumentName",
-    description: "The document name",
-    examples: ["abc123"],
+    description: "A name for the document",
+    examples: ["myDocumentNameHere"],
     expected: (ctx) => {
       switch (ctx.code) {
         case "pattern": {
           return "a valid Base64URL";
         }
         case "minLength": {
-          return `more than ${ctx.rule} characters`;
+          return `at least ${ctx.rule} characters long`;
         }
         case "maxLength": {
-          return `less than ${ctx.rule} characters`;
+          return `at most ${ctx.rule} characters long`;
         }
         default: {
           return "valid";
@@ -26,49 +27,45 @@ export const validatorDocumentName = type(/^[A-Za-z0-9_-]+$/)
     }
   });
 
-export const validatorDocumentNameLength = type.string.pipe(
-  (string) => Number.parseInt(string, 10),
-  type.number
-    .atLeast(constant.documentNameLengthMin)
-    .atMost(constant.documentNameLengthMax)
-    .configure({
-      ref: "DocumentNameLength",
-      description: "The document name length",
-      expected: (ctx) => {
-        switch (ctx.code) {
-          case "domain": {
-            return "a valid integer";
-          }
-          case "min": {
-            return `more than ${ctx.rule} length`;
-          }
-          case "max": {
-            return `less than ${ctx.rule} length`;
-          }
-          default: {
-            return "valid";
-          }
+export const validatorDocumentNameLength = type.keywords.string.integer.parse
+  .to(type.number.atLeast(constant.documentNameLengthMin).atMost(constant.documentNameLengthMax))
+  .configure({
+    ref: "DocumentNameLength",
+    description: "The name length for a document",
+    expected: (ctx) => {
+      switch (ctx.code) {
+        case "domain": {
+          return "a valid integer";
+        }
+        case "min": {
+          return `must be greater than ${ctx.rule}`;
+        }
+        case "max": {
+          return `must be less than ${ctx.rule}`;
+        }
+        default: {
+          return "valid";
         }
       }
-    })
-);
-
-export const validatorDocumentPasswordEmpty = type.string.exactlyLength(0).configure({
-  ref: "DocumentPassword.empty",
-  description: "The password for the document (should only be used to nullify an existing password)",
-  examples: [""]
-});
+    }
+  });
 
 export const validatorDocumentPassword = type.string
   .atLeastLength(constant.documentPasswordLengthMin)
   .atMostLength(constant.documentPasswordLengthMax)
   .configure({
     ref: "DocumentPassword.default",
-    description: "The password for the document",
-    examples: ["myPassword"]
+    description: "A password for the document (for read access)",
+    examples: ["myDocumentPasswordHere"]
   });
+
+export const validatorDocumentPasswordEmpty = type.string.exactlyLength(0).configure({
+  ref: "DocumentPassword.empty",
+  description: "A blank password for the document",
+  examples: [""]
+});
 
 export const validatorDocumentDownload = type.unknown.configure({
   ref: "DocumentDownload",
-  description: "The response will be treated as a file download"
+  description: "Indicate the client that downloads the document as a file attachment (only useful in web browsers)"
 });
