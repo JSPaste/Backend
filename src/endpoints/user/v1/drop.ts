6324536b47728f61aced5cd94586b9ad34425420
Ivan Gabaldon <igabaldon@inetol.net>
2026-04-26 10:13:47 +0200

    rotateToken & drop in user/v1

---
+++ src/endpoints/user/v1/drop.ts
@@ -0,0 +1,40 @@
+import { describeRoute } from "@hono/openapi";
+import { Hono } from "hono/tiny";
+
+import { constantHttpStatusCodes, mutable } from "#/global.ts";
+import type { Env } from "#http/handler.ts";
+import { authMiddleware } from "#http/middleware/authorization.ts";
+import { ErrorCode, errorThrow, genericErrorResponse } from "#util/error.ts";
+
+export default new Hono<Env>().delete(
+  "/",
+  describeRoute({
+    tags: ["USER (v1)"],
+    summary: "Drop user",
+    description: `Deletes a user in the instance
+
+Note: All documents owned by the user will also be deleted`,
+    security: [{ bearer: [] }],
+    responses: {
+      200: {
+        description: constantHttpStatusCodes[200]
+      },
+      400: { ...genericErrorResponse, description: constantHttpStatusCodes[400] },
+      404: { ...genericErrorResponse, description: constantHttpStatusCodes[404] },
+
+      // auth middleware
+      401: { ...genericErrorResponse, description: constantHttpStatusCodes[401] }
+    }
+  }),
+  authMiddleware,
+  (ctx) => {
+    const userId = ctx.get("userId");
+    if (!userId) {
+      return errorThrow(ErrorCode.UserInvalidToken);
+    }
+
+    mutable.database.user.delete("id", userId);
+
+    return ctx.body(null);
+  }
+);
--- src/endpoints/user/v1/index.ts
+++ src/endpoints/user/v1/index.ts
@@ -3,7 +3,11 @@ import { Hono } from "hono/tiny";
 import type { Env } from "#http/handler.ts";
 
 import create from "./create.ts";
+import drop from "./drop.ts";
+import rotateToken from "./rotateToken.ts";
 
 export const v1UserHandler = new Hono<Env>();
 
 v1UserHandler.route("/", create);
+v1UserHandler.route("/", drop);
+v1UserHandler.route("/", rotateToken);
